2c7453ace37907a140a1e6ec5623ccc403f4c4fc
DrinkFireBurnWater <greg31552@gmail.com>
2026-03-05 19:22:36 -0800

    initial styling

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      "./src/templates/**/*.html",
+      "./src/static/js/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
